d53773ac2ad159bdfc540939aa719c56681bc25f
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2022-11-04 10:10:43 +0100

    First commit

---
+++ src/Routes.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+
+import { Home } from './Pages/Home/Home'
+
+export function AppRoutes() {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />}/>
+        {/* <Route path="/listings/:id" element={<Listing />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="*" element={<NotFound />}/> */}
+    </Routes>
+  )
+}
