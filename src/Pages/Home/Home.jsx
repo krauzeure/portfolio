be2ac80e31e4cdea74e6c3120a1f658fd30bf6c1
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2022-11-06 17:07:30 +0100

    Editing the home CTAs

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,26 +6,21 @@ export function Home() {
   return (
     <main class="main-about">
         <section class="main-description">
-            <h1>👋 J'me présente, je m'appelle Cyrielle</h1>
+            <h1><span className="main-description-hello">👋</span> J'me présente, je m'appelle Cyrielle</h1>
             <p>Dev Front, Project Manager, Amatrice de problèmes à résoudre.</p>
         </section>
 
-        <Link class="resume-cta" to="/resume">Voir mon CV</Link>
+        <div className="home-buttons">
+          <Link class="home-cta resume-cta" to="/resume">Voir mon CV</Link>
+          <Link class="home-cta projects-cta" to="/projects">Voir mes projets</Link>
+        </div>
 
         <div class="main-content">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam ipsum officia atque incidunt, molestias eos nam sit beatae earum molestiae rem optio et nemo repellendus distinctio accusantium aliquam quos ipsa.
             Error amet eaque consequuntur, maxime, tenetur optio nesciunt atque et ullam iusto eligendi inventore excepturi, reprehenderit quasi libero ea in velit laudantium ratione modi! Eum minima quo consectetur unde sit!
             Accusantium excepturi commodi voluptatem labore eligendi, laboriosam maxime mollitia accusamus praesentium. Mollitia, quam eum? Corporis vero tenetur, itaque est praesentium nulla sed exercitationem aperiam necessitatibus beatae fugiat fugit! Fuga, quo?
             Officiis saepe quam reprehenderit non perspiciatis placeat? Aut, temporibus aperiam. In quod veniam aliquid, ipsam assumenda consequuntur impedit. Hic in inventore voluptate placeat dicta veniam neque id quia ipsum facere!
-            Iure explicabo quam enim nostrum distinctio quidem delectus perspiciatis aliquid quos ab quaerat ut dolore sint illum, porro error debitis minima temporibus deleniti culpa animi ipsum fugit maxime? Voluptatum, accusantium!
-            Assumenda accusamus suscipit ex. Fugiat voluptate maxime recusandae neque error alias commodi eveniet ratione ea accusamus? Eos quos nostrum odit quam numquam cum accusantium aspernatur itaque, magni porro quod suscipit.
-            Voluptatum, officia provident ut facilis officiis qui quo, voluptatem iusto reprehenderit minus deleniti modi velit pariatur perferendis. Distinctio dignissimos iure voluptates fuga. Nisi asperiores soluta quibusdam dolorum, vel culpa eius.
-            Officia vero placeat quam tenetur inventore, in id necessitatibus adipisci porro repudiandae voluptas, cupiditate labore maxime quaerat neque tempora recusandae cumque nisi? Tempore, amet sunt. Accusamus, alias. Ratione, facere officia!
-            Fuga eos dicta aut, nobis eligendi rem id dolorem. Perspiciatis amet vero, consequuntur iste laboriosam recusandae a ut porro doloremque esse tempore saepe, cum, obcaecati aut fugit quidem sed id.
-            Similique rerum itaque, aliquid iste ut nulla. Accusamus mollitia cumque voluptatum eveniet minima. Vitae animi consequuntur itaque suscipit inventore! Praesentium temporibus iste vitae deleniti cum repellat quam tempore aut nostrum?
-            Repellat impedit quod molestiae cupiditate consequuntur eaque? Amet dolore, et natus qui repellat asperiores sequi voluptatem fugiat consequuntur hic blanditiis voluptates? Excepturi suscipit nihil sapiente, vitae quod facilis earum assumenda.
-            Quia qui voluptatibus aut deleniti repellendus odio et. Dolores laboriosam, placeat, molestiae doloremque quod asperiores tempore iure enim optio impedit tempora dolorum atque pariatur, vel molestias iusto. Repellendus, autem possimus?
-            Repudiandae repellat esse natus molestiae a nostrum quia reiciendis placeat accusantium soluta rem quo optio doloremque iusto alias neque aperiam inventore minus impedit, tempora nisi eligendi maxime corrupti! Doloremque, quidem.
+            Iure explicabo quam enim nostrum distinctio quidem delectus perspiciatis aliquid quos ab quaerat ut dolore sint illum, porro error debitis minima temporibus deleniti culpa animi ipsum fugit maxime? 
         </div>
     </main>
   )
