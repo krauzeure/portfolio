855b7e2e5ec7b75ead1c6b068a377d2d6603d41c
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2022-11-06 17:06:53 +0100

    Editing the social media links to adapt to dark theme

--- src/Components/SocialMedia/SocialMedia.jsx
+++ src/Components/SocialMedia/SocialMedia.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 
-import Github from './github.png'
-import LinkedIn from './linkedin.png'
-import Medium from './medium.png'
+import Github from './github.svg'
+import LinkedIn from './linkedin.svg'
+import Medium from './medium.svg'
 
 import './SocialMedia.css'
 
+import { useContext } from 'react'
+
+import ThemeContext from '../../ThemeContext'
+
 export function SocialMedia() {
+
+  const contextValue = useContext(ThemeContext);
+  console.log(contextValue.theme)
+
   return (
     <div class="social-media-container">
-        <img src={Github} alt="github"/>
-        <img src={LinkedIn} alt="linkedin"/>
-        <img src={Medium} alt="medium"/>
+        <img src={Github} alt="github"
+        className={contextValue.theme ? "social-media-logo" : "social-media-logo social-media-logo-inverted"}/>
+        <img src={LinkedIn} alt="linkedin"
+        className={contextValue.theme ? "social-media-logo" : "social-media-logo social-media-logo-inverted"}/>
+        {/* <img src={Medium} alt="medium" 
+        className={contextValue.theme ? "social-media-logo" : "social-media-logo social-media-logo-inverted"}/> */}
     </div>
   )
 }
