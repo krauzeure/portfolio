df7e60b5d45de68d3015c144d4826857d490bfc5
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2022-11-18 09:31:36 +0100

    New header structure to prepare for burger menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -33,34 +33,36 @@ export function Header() {
   return (
     <header>
         <nav className="header-nav-container">
-            <div className="nav-links">
-                <a href="/"><img 
-                src={contextValue.theme ? Logo : LogoDark}
-                alt="logo" 
-                className="profile-photo"/></a>
-                <ul
-                className={menuState ? "nav-anchors nav-burger-open" : "nav-anchors"}>
-                    <a href="/#projects">
-                        <li className="nav-item fr">{langValue.lang ? frenchCopy.header.projects : englishCopy.header.projects}</li>
-                    </a>
-                    <a href="/#experience">
-                        <li className="nav-item fr">{langValue.lang ? frenchCopy.header.experience : englishCopy.header.experience}</li>
-                    </a>
-                    <a href="/#about">
-                        <li className="nav-item fr">{langValue.lang ? frenchCopy.header.about : englishCopy.header.about}</li>
-                    </a>
-                    <a href="/#contact">
-                        <li className="nav-item fr">Contact</li>
-                    </a>
-                </ul>
-            </div>
-            <div 
-            className={menuState ? "nav-selectors nav-burger-open" : "nav-selectors"}>
-                <div className="language-select nav-selector" onClick={toggleLang}>
-                    {langValue.lang ? frenchCopy.header.lang : englishCopy.header.lang}
+            <a href="/"><img 
+            src={contextValue.theme ? Logo : LogoDark}
+            alt="logo" 
+            className="profile-photo"/></a>
+            <div className="nav-all-links">
+                <div className="nav-links">
+                    <ul
+                    className={menuState ? "nav-anchors nav-burger-open" : "nav-anchors"}>
+                        <a href="/#projects">
+                            <li className="nav-item fr">{langValue.lang ? frenchCopy.header.projects : englishCopy.header.projects}</li>
+                        </a>
+                        <a href="/#experience">
+                            <li className="nav-item fr">{langValue.lang ? frenchCopy.header.experience : englishCopy.header.experience}</li>
+                        </a>
+                        <a href="/#about">
+                            <li className="nav-item fr">{langValue.lang ? frenchCopy.header.about : englishCopy.header.about}</li>
+                        </a>
+                        <a href="/#contact">
+                            <li className="nav-item fr">Contact</li>
+                        </a>
+                    </ul>
                 </div>
-                <div className="theme-select nav-selector" onClick={toggleTheme}>
-                {contextValue.theme ? "🌙" : "☀️"}
+                <div 
+                className={menuState ? "nav-selectors nav-burger-open" : "nav-selectors"}>
+                    <div className="language-select nav-selector" onClick={toggleLang}>
+                        {langValue.lang ? frenchCopy.header.lang : englishCopy.header.lang}
+                    </div>
+                    <div className="theme-select nav-selector" onClick={toggleTheme}>
+                    {contextValue.theme ? "🌙" : "☀️"}
+                    </div>
                 </div>
             </div>
             <div className="nav-burger" onClick={updateMenu}>
