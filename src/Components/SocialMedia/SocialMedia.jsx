da2eab7ffada00658261355719b1dbff2b6ff6da
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-01-18 13:46:49 +0100

    Editing linkedin link

--- src/Components/SocialMedia/SocialMedia.jsx
+++ src/Components/SocialMedia/SocialMedia.jsx
@@ -19,7 +19,7 @@ export function SocialMedia() {
           <img src={Github} alt="github"
           className={contextValue.theme ? "social-media-logo" : "social-media-logo social-media-logo-inverted"}/>
         </a>
-        <a href="https://www.linkedin.com/in/cyrielle-chasles-ab0b3b100/" target="_blank" rel="noreferrer">
+        <a href="https://www.linkedin.com/in/cyrielle-chasles/" target="_blank" rel="noreferrer">
           <img src={LinkedIn} alt="linkedin"
           className={contextValue.theme ? "social-media-logo" : "social-media-logo social-media-logo-inverted"}/>
         </a>
