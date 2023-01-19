fe95cb22bfbb8a77e454e8868e6abc771c8f0448
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-01-19 21:40:51 +0100

    Adding project content in french for kasa and ohmyfood

--- src/Components/Popup/Popup.jsx
+++ src/Components/Popup/Popup.jsx
@@ -7,6 +7,7 @@ import Github from './github.png'
 import Git from './git.png'
 import HTML from './html.png'
 import CSS from './css.png'
+import Sass from './sass.png'
 import Kasa1 from './kasa1.gif'
 import Kasa2 from './kasa2.gif'
 import LangContext from '../../LangContext'
@@ -39,15 +40,64 @@ export function Popup(props) {
             <>
                 <p>J'ai travaillé sur le projet Kasa dans le cadre de ma formation et il s'agit de mon premier projet en React. Ce projet m'a permis de découvrir la librairie, de créer des modules courants, et de <u>me baser sur les maquettes pour créer la meilleure expérience utilisateur</u> possible.</p>
                 <div className="projects-details-images"><img src={Kasa1} alt="gif kasa"/><img src={Kasa2} alt="gif kasa"/></div>
-                <div className="projects-details-main">Mes tâches sur ce projet :
-                <ul>
-                    <li>Intégration des maquettes fournies par le Product Designer</li>
-                    <li>Création d'un slideshow pour afficher les photos des annonces avec activation au touch, au clavier ou au clic</li>
-                </ul>
+                <div className="projects-details-main"><h3>Mon rôle sur ce projet</h3>
+                    <ul>
+                        <li>Intégration des maquettes fournies par le Product Designer (avec hooks et composants)</li>
+                        <li>Intégration <u>dynamique des informations</u> (utilisateurs et listings)</li>
+                        <li>Création d'un slideshow pour afficher les photos des annonces avec activation au touch, au clavier ou au clic</li>
+                        <li>Création d'un <u>collapse à hauteur dynamique</u></li>
+                        <li>Création de <u>plusieurs pages via React Router</u> (dont une page 404)</li>
+                        <li>Mise en ligne de la plateforme via Github pages</li>
+                    </ul>
                 </div>
             </>;
-        } else if(!langValue.lang) {
-            projectDesc = <><p>J'ai travaillé sur le projet Kasa dans le cadre de ma formation et il s'agit de mon premier projet en React. Ce projet m'a permis de découvrir la librairie, de créer des modules courants, et de <u>me baser sur les maquettes pour créer la meilleure expérience utilisateur</u> possible.</p><div className="projects-details-images"><img src={Kasa1} alt="gif kasa"/><img src={Kasa2} alt="gif kasa"/></div></>;
+        } else {
+            projectDesc =
+            <>
+                <p>J'ai travaillé sur le projet Kasa dans le cadre de ma formation et il s'agit de mon premier projet en React. Ce projet m'a permis de découvrir la librairie, de créer des modules courants, et de <u>me baser sur les maquettes pour créer la meilleure expérience utilisateur</u> possible.</p>
+                <div className="projects-details-images"><img src={Kasa1} alt="gif kasa"/><img src={Kasa2} alt="gif kasa"/></div>
+                <div className="projects-details-main"><h3>Mon rôle sur ce projet</h3>
+                    <ul>
+                        <li>Intégration des maquettes fournies par le Product Designer (avec hooks et composants)</li>
+                        <li>Intégration <u>dynamique des informations</u> (utilisateurs et listings)</li>
+                        <li>Création d'un slideshow pour afficher les photos des annonces avec activation au touch, au clavier ou au clic</li>
+                        <li>Création d'un <u>collapse à hauteur dynamique</u></li>
+                        <li>Création de <u>plusieurs pages via React Router</u> (dont une page 404)</li>
+                        <li>Mise en ligne de la plateforme via Github pages</li>
+                    </ul>
+                </div>
+            </>;
+        }
+    } else if(props.project === "Ohmyfood!") {
+        projectStack = [HTML, Sass, Git, Github];
+        if(langValue.lang) {
+            projectDesc =
+            <>
+                <p>J'ai travaillé sur le projet Ohmyfood! dans le cadre de ma formation. Pour ce projet, j'ai utilisé Sass et j'ai travaillé sur les animations CSS pour dynamiser la plateforme. Les maquettes ont été <u>intégrées en mobile first</u>, puis adaptées au format desktop.</p>
+                <div className="projects-details-images"><img src={Kasa1} alt="gif kasa"/><img src={Kasa2} alt="gif kasa"/></div>
+                <div className="projects-details-main"><h3>Mon rôle sur ce projet</h3>
+                    <ul>
+                        <li>Intégration des maquettes en mobile first</li>
+                        <li>Utilisation des variables Sass</li>
+                        <li><u>Création d'animations</u> sur les différentes pages (like, séléction de plat, chargement de la page)</li>
+                        <li>Utilisation et personnalisation des icones fontawesome</li>
+                    </ul>
+                </div>
+            </>;
+        } else {
+            projectDesc =
+            <>
+                <p>J'ai travaillé sur le projet Ohmyfood! dans le cadre de ma formation. Pour ce projet, j'ai utilisé Sass et j'ai travaillé sur les animations CSS pour dynamiser la plateforme. Les maquettes ont été <u>intégrées en mobile first</u>, puis adaptées au format desktop.</p>
+                <div className="projects-details-images"><img src={Kasa1} alt="gif kasa"/><img src={Kasa2} alt="gif kasa"/></div>
+                <div className="projects-details-main"><h3>Mon rôle sur ce projet</h3>
+                    <ul>
+                        <li>Intégration des maquettes en mobile first</li>
+                        <li>Utilisation des variables Sass</li>
+                        <li><u>Création d'animations</u> sur les différentes pages (like, séléction de plat, chargement de la page)</li>
+                        <li>Utilisation et personnalisation des icones fontawesome</li>
+                    </ul>
+                </div>
+            </>;
         }
     }
 
